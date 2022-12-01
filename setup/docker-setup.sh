3ecd102395a3907e85f7e1bfde6219f5a9d57766
rudy-prietno <prietnorudy@gmail.com>
2022-12-01 15:12:45 +0700

    Update docker-setup.sh

--- setup/docker-setup.sh
+++ setup/docker-setup.sh
@@ -14,6 +14,7 @@ echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
+sudo apt-get install docker-ce, docker-compose-plugin
 
 sudo usermod -aG docker ${USER}
 sudo passwd academydataengineer
