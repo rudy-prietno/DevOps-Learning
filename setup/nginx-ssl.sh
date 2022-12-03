fe8b18618bb87ae7782257ff1a53e21d4502c80c
rudy-prietno <prietnorudy@gmail.com>
2022-12-03 08:51:03 +0700

    Update nginx-ssl.sh

--- setup/nginx-ssl.sh
+++ setup/nginx-ssl.sh
@@ -1,46 +1,93 @@
-# install nginx
-sudo apt-get update
-sudo apt-get install nginx
+!/bin/bash
+# This script is intended to setup the environment for a NGINX Web Server with SSL certificate using Let's Encrypt.
 
-# setup nginx
-cd /etc/nginx/sites-available
+# Get latest updates
+echo ""
+echo "UPDATING SYSTEM..."
+echo ""
+sudo apt update
+clear
 
-# define new file & rename like your domain
-- touch stag.aktifitasacak.com
+# Get install nginx
+sudo apt-get -y install nginx
+clear
 
-sudo nano stag.aktifitasacak.com
-# insert this command 
-server {
-       listen 80;
-       listen [::]:80;
-
-       server_name stag.aktifitasacak.com;
-       access_log /var/log/nginx/stag.aktifitasacak.com-access.log;
-       error_log /var/log/nginx/stag.aktifitasacak.com-error.log;
-
-       location / {
-            proxy_set_header Host $host;
-            proxy_set_header X-Real-IP $remote_addr;
-            proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-            proxy_pass http://{replace with IP instance}:8080;
-       }
-}
-
-sudo nginx -t
-
-# copy with symlink
-sudo ln -s /etc/nginx/sites-available/stag.aktifitasacak.com /etc/nginx/sites-enabled/
-
-cd /etc/nginx/sites-enabled
-
-sudo systemctl restart nginx
-tail -f /var/log/nginx/stag.aktifitasacak.com-error.log
-
-
-# https://certbot.eff.org/ setup ssl using certbot
+# install certbot
 sudo snap install core
 sudo snap install --classic certbot
 sudo ln -s /snap/bin/certbot /usr/bin/certbot
-sudo certbot --nginx
+clear
+
+
+# setup reverse proxy & ssl
+PROJECT_DIR_NGINX_AVAILABLE="/etc/nginx/sites-available"
+PROJECT_DIR_NGINX_ENABLED="/etc/nginx/sites-enabled"
+CURRENT_DIR=$(pwd)
+
+cd ${PROJECT_DIR_NGINX_AVAILABLE}
+pwd
+
+# frontend
+sudo rm -rf ${PROJECT_DIR_NGINX_AVAILABLE}/stag.domain.com
+sudo rm -rf ${PROJECT_DIR_NGINX_ENABLED}/stag.domain.com
+
+sudo touch ${PROJECT_DIR_NGINX_AVAILABLE}/stag.domain.com
+
+echo 'server {
+            server_name stag.domain.com;
+            
+            access_log /var/log/nginx/stag.domain.com-access.log;
+            error_log /var/log/nginx/stag.domain.com-error.log;
+
+            location / {
+                proxy_set_header Host $host;
+                proxy_set_header X-Real-IP $remote_addr;
+                proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+                proxy_pass http://IP:8081;
+            }
+        }' | sudo tee ${PROJECT_DIR_NGINX_AVAILABLE}/stag.domain.com
+
+# copy with symlink
+sudo ln -s ${PROJECT_DIR_NGINX_AVAILABLE}/stag.domain.com ${PROJECT_DIR_NGINX_ENABLED}/
+
+# setup certbot
+sudo certbot --nginx certonly -n -d stag.domain.com
+
+
+# backend
+sudo rm -rf ${PROJECT_DIR_NGINX_AVAILABLE}/api.domain.com
+sudo rm -rf ${PROJECT_DIR_NGINX_ENABLED}/api.domain.com
+
+sudo touch ${PROJECT_DIR_NGINX_AVAILABLE}/api.domain.com
+
+echo 'server {
+            server_name api.domain.com;
+            
+            access_log /var/log/nginx/api.domain.com-access.log;
+            error_log /var/log/nginx/api.domain.com-error.log;
+
+            location / {
+                proxy_set_header Host $host;
+                proxy_set_header X-Real-IP $remote_addr;
+                proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+                proxy_pass http://IP:8080;
+            }
+        }' | sudo tee ${PROJECT_DIR_NGINX_AVAILABLE}/api.domain.com
+
+# copy with symlink
+sudo ln -s ${PROJECT_DIR_NGINX_AVAILABLE}/api.domain.com ${PROJECT_DIR_NGINX_ENABLED}/
+
+# check status nginx config
+sudo nginx -t
+
+# setup certbot
+sudo certbot --nginx -n -d api.domain.com
+sudo certbot --nginx -n -d stag.domain.com
+
+# restart nginx 
 sudo systemctl restart nginx
-sudo service nginx configtest && sudo service nginx reload
+clear
+
+echo ""
+echo "INSTALLING NGINX && SSL DONE"
+echo ""
